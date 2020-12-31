44e02b5bc61eb28263d1747a9c38fdc0f832bc97
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2020-12-31 10:45:34 -0300

    Update scriptE3.js

--- Teste estágio 2020 - nave.rs/Exercício 3/scriptE3.js
+++ Teste estágio 2020 - nave.rs/Exercício 3/scriptE3.js
@@ -1,8 +1,5 @@
 /*E.3 Crie uma função que recebe vários argumentos do tipo string e imprime todos juntos*/
 
-function imprime(arg1, arg2, arg3, arg4, arg5, arg6){
-    var concatena = (arg1.concat(arg2, arg3, arg4, arg5, arg6)).toString()
-    console.log(concatena)
-}
+const imprime = (...args) =>  args.reduce((args1, args2) => args1.concat(args2))
 
 imprime("Olá", ",", " este é um ", "exercício", " em javascript", "!") 
